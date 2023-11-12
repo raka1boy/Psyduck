de2690f4261e54a139aed236ce69ada386adc16b
Chernyshov Artem <artemka2015kek@gmail.com>
2023-11-12 18:51:42 +0300

    properly inited db and made some minimal code coverages

--- kursovich/src/Products.jl
+++ kursovich/src/Products.jl
@@ -17,6 +17,7 @@ module Products
         </li>
       </template>"""
     end
+    
     Base.@kwdef struct Product
         id::Union{String,Integer} = "NULL"
         name::String
@@ -32,7 +33,7 @@ module Products
             id,result[1, "name"],result[1, "image_link"],result[1, "warehouse_id"],result[1, "description"],result[1, "price"]
         )
     end
-
+    #make :ascend :descend :popular :least_popular etc.
     get_products(limit)::Vector{Product} = begin
         result = DB.execute_query("SELECT * FROM products LIMIT $(limit)")
         [Product(result[i, "id"],result[i, "name"],result[i, "image_link"],result[i, "warehouse_id"],result[i, "description"],result[i, "price"]) for i in 1:limit]
--- kursovich/src/Users.jl
+++ kursovich/src/Users.jl
@@ -1,3 +1,13 @@
 module Users
-using Kursovich.DB
+    using Kursovich.DB
+    
+    Base.@kwdef struct User
+        id::Union{Integer,String} = "NULL"
+        name::String
+        image_link::String
+        login::String
+        password::String
+    end
+
+    
 end
